2bc3b4fa3838f13e78fa6d9aed9573f95576fe3c
Intronet <nigelfrancois@hotmail.com>
2017-02-26 15:32:14 +0000

    new

---
+++ index.js
@@ -0,0 +1,58 @@
+var
+   app = require('express')(),
+   http = require('http').Server(app),
+   io = require('socket.io')(http),
+   conversation = [],
+   users = [];
+
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+
+   socket.on('logout', function (msg) {
+      if (users.indexOf(msg.user) !== -1) {
+         var index = users.indexOf(msg.user);
+         users.splice(index, 1);
+         conversation.push(msg);
+         io.emit('chat message', msg, JSON.stringify(conversation), JSON.stringify(users));
+         //io.emit('logout', msg, JSON.stringify(conversation), JSON.stringify(users));
+      }
+   });
+   
+   socket.on('checkuser', function (msg) {
+      if (users.indexOf(msg.user) !== -1) {
+         msg = {
+            "usersExists": true,
+            user: msg.user,
+            id: msg.id
+         };
+      } else {
+         users.push(msg.user);       
+      }
+      io.emit('checkuser', msg, JSON.stringify(conversation), JSON.stringify(users));
+   });
+
+   socket.on('chat message', function (msg) {
+      if (users.indexOf(msg.user) !== -1) {
+         conversation.push(msg);
+         if (conversation.length > 100){
+            conversation.splice(0, conversation.length-100);
+            }         
+         io.emit('chat message', msg, JSON.stringify(conversation), JSON.stringify(users));
+      } else {
+         msg = {
+            "invalid": true,
+            user: msg.user,
+            id: msg.id
+         }
+         io.emit('chat message', msg, JSON.stringify(conversation), JSON.stringify(users));
+      } 
+   });
+  
+});
+
+http.listen(8888, function () {
+   console.log('listening on *:8888');
+});
